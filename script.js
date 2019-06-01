4524d12a1a1d096ea13379ed4193e062d17331e2
student <student@admins-MacBook-Pro.local>
2019-06-01 10:11:14 -0700

    updated

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 $(document).ready(function(){
 
 
+
+let leftPokemon;
+
 //starter function to pull a random pokemon from the PokeAPI
   function getRandomPokemonLeft() {
     let randomNumber = Math.floor(Math.random() * 800) + 1;
     $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
-      console.log("Left Pokemon Data: ", pokeData);
+      //console.log("Left Pokemon Data: ", pokeData);
+      leftPokemon = pokeData
+      console.log(leftPokemon)
+      console.log(leftPokemon['name'])
+      console.log(leftPokemon['sprites']['front_default'])
+
+      let leftPokemonDisplay = `
+      <h2>${leftPokemon['name']}</h2>
+      <img src = "${leftPokemon['sprites']['front_default']}">
+ `
+      console.log(leftPokemonDisplay)
+$("#left").empty();
+$("#left").append(leftPokemonDisplay);
+
+
+
     });
   }
 
+$("#leftButton").click(getRandomPokemonLeft)
 
 
-//all code here
+function getRandomPokemonRight() {
+  let randomNumber = Math.floor(Math.random() * 800) + 1;
+  $.get(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`).then(function(pokeData){
+    //console.log("Right Pokemon Data: ", pokeData);
+    rightPokemon = pokeData
+    console.log(rightPokemon)
+    console.log(rightPokemon['name'])
+    console.log(rightPokemon['sprites']['front_default'])
+
+    let rightPokemonDisplay = `
+    <h2>${rightPokemon['name']}</h2>
+    <img src = "${rightPokemon['sprites']['front_default']}">
+`
+    console.log(rightPokemonDisplay)
+$("#right").empty();
+$("#right").append(rightPokemonDisplay);
+
+
+
+  });
+}
+
+$("#rightButton").click(getRandomPokemonRight)
 
 
 
